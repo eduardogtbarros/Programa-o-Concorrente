7645408c2085fb36b2205847ebd169b81ae5160f
eduardogtbarros <eduardobarrospro@gmail.com>
2024-09-30 23:01:35 -0300

    REFACTOR: Finalizando o exercício 7

--- lista1/ex7.c
+++ lista1/ex7.c
@@ -15,6 +15,7 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <pthread.h>
 
 #define TAM 5 //tamanho do buffer
@@ -57,7 +58,7 @@ void* insere(){
 * @return Retorna o elemento retirado.
 */
 void* retira(){
-    int elemento; //elemento a ser retirado do buffer
+    int* elemento; //elemento a ser retirado do buffer
     
     //entra na seção crítica
     pthread_mutex_lock(&mutex);
@@ -68,7 +69,7 @@ void* retira(){
     //decrementa o contador de elementos do buffer
     contador--;
     //retira o elemento do buffer
-    elemento = buffer[i_sai];
+    *elemento = buffer[i_sai];
     //incrementa o índice do elemento
     i_sai = (i_sai+1)%TAM;
     //sai da seção crítica
@@ -82,5 +83,48 @@ void* retira(){
 }
 
 int main(int argc, char *argv[]){
+    int i;
+    int produtores, consumidores;
+    int* retorno;
+    pthread_t *t_prod, *t_cons;
+    srand(time(NULL));
+
+    if(argc<3){
+        printf("Digite: %s <nº de produtores> <nº de consumidores>\n", argv[0]);
+        return 1;
+    }
+    //Recebe a quantidade de termos
+    produtores = atoi(argv[1]);
+    //Recebe a quantidade de threads
+    consumidores = atoi(argv[2]);
+
+    t_prod = malloc(sizeof(pthread_t)*produtores);
+    t_cons = malloc(sizeof(pthread_t)*consumidores);
+
+    for(i=0;i<produtores;i++){
+        if(pthread_create(&t_prod[i], NULL, insere, NULL)){
+            printf("Erro ao criar thread."); exit(-1);
+        }
+    }
+    for(i=0;i<consumidores;i++){
+        if(pthread_create(&t_cons[i], NULL, retira, NULL)){
+            printf("Erro ao criar thread."); exit(-1);
+        }
+    }
+    
+    for(i=0;i<produtores;i++){
+        if(pthread_join(t_prod[i], NULL)){
+            printf("Erro ao receber retorno da thread."); exit(-1);
+        }
+    }
+    for(i=0;i<consumidores;i++){
+        if(pthread_join(t_cons[i], (void *) &retorno)){
+            printf("Erro ao receber retorno da thread."); exit(-1);
+        }
+        printf("Consumido: %d\n",*retorno);
+    }
+    
+    free(t_prod);
+    free(t_cons);
     return 0;
 }
