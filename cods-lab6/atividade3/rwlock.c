10f5d2f9205089a6557cfa162027e98f3596fa07
Eduardo Barros <eduardogtb@dcc.ufrj.br>
2024-10-03 09:18:27 -0300

    FEAT: Preparando o laboratório 6

---
+++ cods-lab6/atividade3/rwlock.c
@@ -0,0 +1,15 @@
+/* Disciplina: Programação Concorrente
+   Profa.: Silvana Rossetto
+   Laboratório: 6
+   Autor: Eduardo Barros 
+
+   Implementar o padrão leitores e escritores com prioridade para escrita.
+   Implemente sua versão de rwlock com prioridade para operações de escrita.
+   Isso significa que sempre que uma operação de escrita for solicitada,
+   novas operações de leitura não poderão começar (mesmo que outras operações de 
+   leitura já estejam acontecendo), até que a operação de escrita seja atendida.
+*/
+
+int main(int argc, char* argv[]){
+    return 0;
+}
