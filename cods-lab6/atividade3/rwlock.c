0a0f841f5fb0d020afff4f3f38fa685066fa16f6
Eduardo Barros <eduardogtb@dcc.ufrj.br>
2024-10-08 13:08:10 -0300

    REFACTOR: Preenchendo as funções de rwlock

--- cods-lab6/atividade3/rwlock.c
+++ cods-lab6/atividade3/rwlock.c
@@ -9,7 +9,62 @@
    novas operações de leitura não poderão começar (mesmo que outras operações de 
    leitura já estejam acontecendo), até que a operação de escrita seja atendida.
 */
+#include <pthread.h>
 
-int main(int argc, char* argv[]){
+int leit=0, escr=0; //contadores de leitura e escrita
+pthread_mutex_t mutex; //variável de lock para exclusão mútua
+pthread_cond_t cond_leit, cond_escr; //variável de condição para a exclusão mútua
+
+/**
+* @brief Função que faz a entrada na leitura, se não houver escrita sendo realizada.
+* @return Contador de leitores incrementado.
+*/
+void read_lock(){
+    pthread_mutex_lock(&mutex);
+    while(escr>0){
+        pthread_cond_wait(&cond_escr,&mutex);
+    }
+    leit++;
+    pthread_mutex_unlock(&mutex);
+}
+
+//------------------X-atualizar para se adaptar ao problema-X-------------------------//
+/**
+* @brief Função que faz a saída da leitura, liberando a escrita se não houver leitores.
+* @return Contador de leitores decrementado.
+*/
+void read_unlock(){
+    pthread_mutex_lock(&mutex);
+    leit--;
+    if(leit==0) pthread_cond_signal(&cond_escr);
+    pthread_mutex_unlock(&mutex);
+}
+
+/**
+* @brief Função que faz a entrada na escrita, se não houver escrita ou leitura sendo realizada.
+* @return Contador de escritores incrementado.
+*/
+void write_lock(){
+    pthread_mutex_lock(&mutex);
+    while((leit>0)||(escr>0)){
+        pthread_cond_wait(&cond_escr, &mutex);
+    }
+    escr++;
+    pthread_mutex_unlock(&mutex);
+}
+
+/**
+* @brief Função que faz a saída da escrita, liberando a escrita e todas as leituras aguardando.
+* @return Contador de leitores decrementado.
+*/
+void write_unlock(){
+    pthread_mutex_lock(&mutex);
+    escr--;
+    pthread_cond_signal(&cond_escr);
+    pthrad_cond_broadcast(&cond_leit);
+    pthread_mutex_unlock(&mutex);
+}
+
+int main (int argc, char* argv[]){
     return 0;
 }
